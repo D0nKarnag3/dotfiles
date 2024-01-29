1c4e6219ea3986c14fc5cdb97362494ef5a6cee7
Lasse Sørensen <1623724+D0nKarnag3@users.noreply.github.com>
2024-01-29 21:43:18 +0100

    Updated telescope keymaps with descriptions

--- nvim/.config/nvim/lua/bawsen/plugins/telescope.lua
+++ nvim/.config/nvim/lua/bawsen/plugins/telescope.lua
@@ -14,12 +14,12 @@ return {
     -- set keymaps
     local keymap = vim.keymap
 
-    keymap.set('n', '<leader>fe', '<CMD>lua require("telescope.builtin").file_browser{ cwd = vim.fn.expand("%:p:h") }<CR>')
-    keymap.set('n', '<leader>fg', '<CMD>lua require("telescope.builtin").git_files{}<CR>')
-    keymap.set('n', '<leader>ff', '<CMD>lua require("telescope.builtin").find_files{ hidden = true }<CR>')
-    keymap.set('n', '<leader>fl', '<CMD>lua require("telescope.builtin").live_grep()<CR>')
-    keymap.set('n', '<leader>fb', '<CMD>lua require("telescope.builtin").buffers()<CR>')
-    keymap.set('n', '<leader>fh', '<CMD>lua require("telescope.builtin").help_tags()<CR>')
-    keymap.set('n', '<leader>fd', '<CMD>lua require("telescope.builtin").lsp_workspace_diagnostics()<CR>')
+    keymap.set('n', '<leader>fe', '<CMD>lua require("telescope.builtin").file_browser{ cwd = vim.fn.expand("%:p:h") }<CR>', { desc = 'File browser' })
+    keymap.set('n', '<leader>fg', '<CMD>lua require("telescope.builtin").git_files{}<CR>', { desc = 'Git files' })
+    keymap.set('n', '<leader>ff', '<CMD>lua require("telescope.builtin").find_files{ hidden = true }<CR>', { desc = 'Find files' })
+    keymap.set('n', '<leader>fl', '<CMD>lua require("telescope.builtin").live_grep()<CR>', { desc = 'Live grep' })
+    keymap.set('n', '<leader>fb', '<CMD>lua require("telescope.builtin").buffers()<CR>', { desc = 'Find buffers' })
+    keymap.set('n', '<leader>fh', '<CMD>lua require("telescope.builtin").help_tags()<CR>', { desc = 'Help tags' })
+    keymap.set('n', '<leader>fd', '<CMD>lua require("telescope.builtin").lsp_workspace_diagnostics()<CR>', { desc = 'Workspace diagnostics' })
   end
 }
