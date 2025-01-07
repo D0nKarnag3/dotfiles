326968330b4eefdcaa73285deab0ec60d55966c0
Lasse Sørensen <1623724+D0nKarnag3@users.noreply.github.com>
2025-01-07 22:05:49 +0100

    feat: Updated configuration for azure devops plugin to display icon for work item types and removed keymaps since these come by default with the plugin

--- nvim/.config/nvim/lua/plugins/azure_devops.lua
+++ nvim/.config/nvim/lua/plugins/azure_devops.lua
@@ -10,18 +10,8 @@ return {
       personal_access_token = os.getenv("AZURE_PAT"),
       organization = os.getenv("AZURE_ORG"),
       project = os.getenv("AZURE_PROJ"),
+      show_work_item_icon = true,
       enable_work_item_details_auto_command = true
     })
-
-    -- set keymaps
-    local keymap = vim.keymap -- for conciseness
-
-    keymap.set("n", "<leader>as", function()
-        require('azure_devops').prompt_search_work_items()
-    end, { noremap = true, silent = true, desc = "Azure DevOps Search Work Items" })
-    keymap.set("n", "<leader>ad", ':AzureGetWorkItem<CR>', { noremap = true, silent = true, desc = "Azure DevOps show detailed informail of work item" })
-    keymap.set("n", "<leader>av", ':AzureShowWorkItemVirtualText<CR>', { noremap = true, silent = true, desc = "Azure DevOps Add work item details" })
-    keymap.set("n", "<leader>ac", ':AzureClearWorkItemVirtualText<CR>', { noremap = true, silent = true, desc = "Azure DevOps Open work item in browser" })
-    keymap.set("n", "<leader>ao", ':AzureOpenWorkItemInBrowser<CR>', { noremap = true, silent = true, desc = "Azure DevOps Open work item in browser" })
   end
 }
